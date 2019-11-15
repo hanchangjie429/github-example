25fe42c758c87710c1de9303f070e073ae959278
hanchangjie429 <hanchangjie429@outlook.com>
2019-11-15 12:55:35 -0500

    introduction

--- week4-intro.py
+++ week4-intro.py
@@ -1,7 +1,5 @@
 '''
 Introduction
 
-ABC is a company based in Toronto for organizing events, currently it works on a project to organize an event for 7 days for a group of photographers from all over the world.
-The company has to put a good program, including a hotel of residence, a hall for meetings, places of landscape to visit, stores for shopping, restaurants and cafes.
-So the company’s purpose is to make a list of places of landscape in Toronto, including the nearest restaurants, cafes, and shopping stores for each place.
+Where would you recommend an art adventurer to visit in order to fulfill her/his hunger for various art pieces? The world is full of wonders and maybe the most ambiguous ones are considered as art. Many people dedicated themselves to travel around the world and discover these ambiguities. This project seeks for art related venues in the capitals of all countries, cluster them and reveals the different cities in terms of art venues although they have closer geography. By the nature of this project, travel companies and their costumers might be interested in this project to find similar and different places around the world.
 '''
