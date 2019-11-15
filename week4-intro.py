c293758d130fc5d7abad170489eabc35a441b370
George Han <43318012+hanchangjie429@users.noreply.github.com>
2019-11-15 13:03:44 -0500

    week4-intro.py
    introduction

--- week4-intro.py
+++ week4-intro.py
@@ -1,4 +1,3 @@
-
 Introduction
 
 Where would you recommend an art adventurer to visit in order to fulfill her/his hunger for various art pieces? The world is full of wonders and maybe the most ambiguous ones are considered as art. Many people dedicated themselves to travel around the world and discover these ambiguities. This project seeks for art related venues in the capitals of all countries, cluster them and reveals the different cities in terms of art venues although they have closer geography. By the nature of this project, travel companies and their costumers might be interested in this project to find similar and different places around the world.
